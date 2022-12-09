3949bb3d8a37f1c2dac756c5026afae4f53dc536
Installshi3ld <thomas.wawrysko@gmail.com>
2022-12-09 11:16:44 +0100

    Smooth Movement PlayerControler

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,32 +5,62 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     int currentRoadIndex = 0;
+    public float lerpDuration = 2f;
+
+    Vector3 positionToMoveTo;
+    bool movementIsFinished = true;
+
     private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentRoadIndex > -1 && Input.GetKeyDown(KeyCode.Q))
+        if (movementIsFinished && currentRoadIndex > -1 && Input.GetKeyDown(KeyCode.Q))
         {
             SubstractActorLocationX(5);
+
+            //SubstractActorLocationX(5);
         }
-        if (currentRoadIndex < 1 && Input.GetKeyDown(KeyCode.D))
+        if (movementIsFinished && currentRoadIndex < 1 && Input.GetKeyDown(KeyCode.D))
         {
             AddActorLocationX(5);
         }
     }
 
+
+    
+    IEnumerator LerpPosition(Vector3 targetPosition, float duration)
+    {
+        movementIsFinished = false;
+        float time = 0;
+        float t ;
+        Vector3 startPosition = transform.position;
+        while (time < duration)
+        {
+            t = time / duration;
+            //Smooth Step Lerp Math Formula
+            t = t * t * (3f - 2f * t);
+            transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+        movementIsFinished = true;
+    }
+
     void AddActorLocationX(int index)
     {
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x + index, 0, 0);
+        positionToMoveTo = new Vector3(gameObject.transform.position.x + index, 0, 0);
+        StartCoroutine(LerpPosition(positionToMoveTo, lerpDuration));
         currentRoadIndex++;
     }
     void SubstractActorLocationX(int index)
     {
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x - index, 0, 0);
+        positionToMoveTo = new Vector3(gameObject.transform.position.x - index, 0, 0);
+        StartCoroutine(LerpPosition(positionToMoveTo, lerpDuration));
         currentRoadIndex--;
     }
 
